12d55ba3a0f30b6b05c57d20d6387cc610c463a5
Shivam <patelsd96@gmail.com>
2020-02-04 23:19:38 +0000

    added another set of queries that focus on using join

---
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -0,0 +1,7 @@
+
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON assignment_submissions.student_id = students.id
+WHERE end_date is NULL
+GROUP BY student
+ORDER BY  average_assignment_duration DESC
