18744b72789daea472cc8a91f9ef199c549c4f0c
Shivam <patelsd96@gmail.com>
2020-02-06 19:21:07 +0000

    added user input and access to the psql databse uusing node postgres libary to use powerful sql queries and then use them in the js code

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+let input = process.argv.slice(2);
+//console.log(input)
+
+const pool = new Pool ({
+  user: 'vagrant', 
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+})
+
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name='${input[0] || 'JUL02'}'
+ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
